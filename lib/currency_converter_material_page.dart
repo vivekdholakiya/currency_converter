a5ef02f44f859f92187ad015ddce3b10f9b9ee8e
Vivek Dholakiya <97219153+vivekdholakiya@users.noreply.github.com>
2023-09-16 11:34:38 +0530

    first commit

---
+++ lib/currency_converter_material_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatelessWidget {
+  const CurrencyConverterMaterialPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '0',
+              style: TextStyle(
+                  fontSize: 55,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 255, 255, 255)),
+            ),
+            TextField(
+              style: TextStyle(
+                color: Colors.black,
+              ),
+              decoration: InputDecoration(
+                  hintText: 'Please enter the amount USD',
+                  hintStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: Color.fromARGB(205, 255, 255, 255),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                        width: 2.0,
+                        style: BorderStyle.solid,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(10vb)))),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'currency_converter_material_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CurrencyConverterMaterialPage(),
+    );
+  }
+}
